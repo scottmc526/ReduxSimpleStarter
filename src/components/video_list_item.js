c32a10d61cb68d35ee8789c9b36c8afff1918c49
Scott Christensen <scottmc526@gmail.com>
2016-07-18 23:04:16 -0600

    video item refactor

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,18 +1,19 @@
 import React from 'react'
 
 const VideoListItem = ({video}) => {
-  console.log(video);
+  const imageUrl = video.snippet.thumbnails.default.url;
+  const title = video.snippet.title
   //passing a property in arguments obj is same as saying:
   //const video = props.video
   return  (
       <li className='list-group-item'>
         <div className='video-list media'>
           <div className='media-left'>
-            <img className='media-object' src={video.snippet.thumbnails.default.url} />
+            <img className='media-object' src={imageUrl} />
           </div>
 
           <div className='media-body'>
-            <div className='media-heading'>{video.snippet.description}</div>
+            <div className='media-heading'>{title}</div>
           </div>
         </div>
       </li>
