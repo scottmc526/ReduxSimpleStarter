7d09dc542f491bfda07203dd60231f94f0fbbdd7
Scott Christensen <scottmc526@gmail.com>
2016-07-18 23:00:56 -0600

    video items

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,22 @@
 import React from 'react'
 
-const VideoListItem = (props) => {
-  return <li>Video</li>
+const VideoListItem = ({video}) => {
+  console.log(video);
+  //passing a property in arguments obj is same as saying:
+  //const video = props.video
+  return  (
+      <li className='list-group-item'>
+        <div className='video-list media'>
+          <div className='media-left'>
+            <img className='media-object' src={video.snippet.thumbnails.default.url} />
+          </div>
+
+          <div className='media-body'>
+            <div className='media-heading'>{video.snippet.description}</div>
+          </div>
+        </div>
+      </li>
+  );
 }
 
 export default VideoListItem
