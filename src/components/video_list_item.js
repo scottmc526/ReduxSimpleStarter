bfc23c51567bfaeb9c4cdc8eef6c73dc53ae0f91
Scott Christensen <scottmc526@gmail.com>
2016-07-19 01:01:31 -0600

    on video select

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
+  console.log(video);
   const imageUrl = video.snippet.thumbnails.default.url;
   const title = video.snippet.title
   //passing a property in arguments obj is same as saying:
   //const video = props.video
   return  (
-    <li className='list-group-item'>
+    <li onClick={() => onVideoSelect(video)} className='list-group-item'>
       <div className='video-list media'>
         <div className='media-left'>
           <img className='media-object' src={imageUrl} />
