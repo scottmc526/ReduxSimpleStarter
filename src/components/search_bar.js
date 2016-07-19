1a192d33c3a30e17d1ce803ddc7d3ed8c2a8a2e0
Scott Christensen <scottmc526@gmail.com>
2016-07-18 22:36:35 -0600

    video list function based component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class SearchBar extends React.Component{
+class SearchBar extends Component{
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' } // term - search term
+  }
+
   render() {
-    return <input />;
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value})}
+          className='search' />
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar'
-
+import VideoList from './components/video_list'
 const API_KEY = 'AIzaSyA3rJ41uVetgnXr50rOb8G26i1TM8or46E';
+
 //Create a new component
 //Component should produce html
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'bowling'}, (videos) => {
+      this.setState({ videos }); //this.setState({videos:videos})
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    )
+  }
 }
 
 //Take this component's generated HTML and put it in the DOM (on the page)
